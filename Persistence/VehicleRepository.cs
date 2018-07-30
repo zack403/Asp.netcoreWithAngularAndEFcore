e40c871f15a33fc897d6572fcfac00424e56df9b
zack <aminuzack7@gmail.com>
2018-07-30 01:10:32 +0100

    code refactoring with the repository pattern

---
+++ Persistence/VehicleRepository.cs
@@ -0,0 +1,25 @@
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Zaap.Models;
+
+namespace Zaap.Persistence
+{
+    public class VehicleRepository
+    {
+        private readonly ZaapDbContext context;
+        public VehicleRepository(ZaapDbContext context)
+        {
+            this.context = context;
+
+        }
+        public async Task<Vehicle> GetVehicle(int id)
+        {
+            return await context.Vehicles
+                     .Include(v => v.Features)
+                     .ThenInclude(vf => vf.Feature)
+                     .Include(v => v.Model)
+                     .ThenInclude(m => m.Make)
+                     .SingleOrDefaultAsync(v => v.Id == id);
+        }
+    }
+}
